fdc657aaaab1049a21cc340215260ffb12cd41b2
Savannah Smith <s.ella@me.com>
2024-10-08 20:39:08 -0600

    feat: Clean up nav, add pages

---
+++ app/components/headerNav.tsx
@@ -0,0 +1,27 @@
+import styles from '../ui/header.module.css';
+import { SVG_Button } from '../ui/SVG/SVG_button';
+
+export default function HeaderNav() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.headerColumn}>
+                <div className={styles.wanderco}>
+                    wanderco
+                </div>
+            </div>
+            <div className={styles.headerColumn}>
+                <div className={styles.headerLinks}>
+                    <a href="/examples" className="headerLink">
+                        See examples
+                    </a>
+                    <a href="/login" className="headerLink">
+                        Log in
+                    </a>
+                    <div> 
+                        <SVG_Button className={styles.icon2} />
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
---
+++ app/examples/page.tsx
@@ -0,0 +1,10 @@
+import FooterNav from "../components/footerNav";
+import HeaderNav from "../components/headerNav";
+
+export default function Page() {
+    return (
+    <div className="examples">
+        <HeaderNav />
+        <div>This page is in progress </div>
+    </div>)
+}
