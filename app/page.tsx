73a8ace19909031eda44f09016f3e6b357f49686
Savannah Smith <s.ella@me.com>
2024-10-08 20:38:56 -0600

    feat: Clean up nav, add pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import FooterNav from "./footerNav";
-import HeaderNav from "./headerNav";
+import FooterNav from "./components/footerNav";
+import HeaderNav from "./components/headerNav";
+import classes from "./ui/landing.module.css";
 
 export default function Home() {
   return (
     <div className="root">
+      <div className={classes.blur}>
+          <img className={classes.backgroundImage} alt="background img" loading="lazy" src="steve-johnson.jpg" />
+      </div>
       <HeaderNav />
+      <div className={classes.landingpage}>
+        <div className={classes.landingContent}>
+          <h1 className={classes.title}>WanderCo</h1>
+          <p className={classes.description}>Helping travel companies filter and find booking clients</p>
+          <a href="/signup" className={classes.button}>Start my 30-day free trial now</a>
+        </div>
+      </div>
+      <div className={classes.slash}></div>
       <FooterNav />
     </div>
   );
